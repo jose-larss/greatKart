f7de417403243b0487b049ee9b0632e0bec16dd3
jlcastro <joselucianocastro10@gmail.com>
2024-09-05 13:24:24 +0200

    implemento login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm
 from accounts.models import Account
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "POST":
@@ -21,14 +23,36 @@ def register(request):
                                                )
             user.phone_number = phone_number
             user.save()
+            
+            messages.success(request, "Registration Successfull")
+            return redirect('register')
     else:
         form = RegistrationForm()
     return render(request, 'accounts/register.html', {'form':form})
 
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+        print(user)
+        if user is not None:
+            auth.login(request, user)
+
+            messages.success(request, "Your are now Login")
+            print('pasa por login')
+            return redirect('home') #actualmente aqui mas tarde ira a dashboard
+        else:
+            messages.error(request, "Invalid Login credentials")
+            return redirect('login')
+
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    messages.success(request, "You are Logged Out")
+    return redirect('login')
